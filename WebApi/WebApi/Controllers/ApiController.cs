8325d2af653128b40acc2494b67a02e55f942691
David Richmond Soto <luis.richmond9@gmail.com>
2022-09-03 23:12:49 -0600

    Web Api
    Inicio Web Api

---
+++ WebApi/WebApi/Controllers/ApiController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Data_base;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+
+        private JsonManager jsonManager = new JsonManager();
+
+        [HttpGet]
+        [Route("requestWorker")]
+        public dynamic RequesteWorker()
+        {
+            return new
+            {
+                id = "1",
+                correo = "google@gmail.com",
+                edad = "19"
+            };
+        }
+
+
+        [HttpPost]
+        [Route("saveWorker")]
+        public dynamic SaveWorker(Worker worker)
+        {
+            worker.idNumber = 5;
+
+            jsonManager.SaveWorker (worker);
+
+            return new
+            {
+                success = true,
+                message = "worker saved",
+                result = worker
+            };
+        }
+
+
+    }
+}
---
+++ WebApi/WebApi/Data base/JsonManager.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using WebApi.Models;
+using System;
+using System.IO;
+using System.Text;
+
+namespace WebApi.Data_base
+{
+    public class JsonManager
+    {
+        private static string path = @"..\Data base\";
+        private JsonSerializer serializer = new JsonSerializer();
+
+
+        public void SaveWorker(Worker worker)
+        {
+            string fullPath = path + "workers.json";
+
+            string output = JsonConvert.SerializeObject(worker);
+
+            File.WriteAllText(fullPath, output);
+
+        }
+        
+    }
+}
