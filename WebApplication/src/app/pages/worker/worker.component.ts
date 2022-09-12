3c1d257804f49e50bdcbb7cefd0573c6beb4620e
David Richmond Soto <luis.richmond9@gmail.com>
2022-09-12 12:08:22 -0600

    web application

---
+++ WebApplication/src/app/pages/worker/worker.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-worker',
+  templateUrl: './worker.component.html',
+  styleUrls: [
+
+  ]
+})
+export class WorkerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
